60e9fd522a9d28763d900005c746901dde38c477
vaansuit <vvansuit@outlook.com>
2022-01-15 21:55:14 -0300

    playable, but not finished

--- js/pong.js
+++ js/pong.js
@@ -1,5 +1,6 @@
 let ctx, p1_y, p2_y, p1_points, p2_points
-let ball_y_orientation, ball_x_orientatio, ball_x, ball_y
+let ball_y_orientation, ball_x_orientation, ball_x, ball_y
+let  p1_key, p2_key
 const h=500, w=800, p_w=20, p_h=80, p1_x =10, p2_x= w - p_w -10
 
 function setup () {
@@ -23,6 +24,50 @@ function setup () {
 }
 
 function loop() {
+
+    //See if the ball is coliding with p1
+
+    if (ball_x >= p1_x && ball_x <= p1_x + 10 && ball_y >= p1_y && ball_y <= p1_y + p_h) {
+        ball_x_orientation =1
+    }
+
+    //Same thing but p2
+    
+    else if (ball_x >= p2_x && ball_x <= p2_x + 10 && ball_y >= p2_y && ball_y <= p2_y + p_h) {
+        ball_x_orientation = -1
+    }
+
+    //See if tha ball touch in the floor/roof
+
+    if (ball_y + 10 >= h || ball_y <= 0) ball_y_orientation *= -1
+
+    //Move the ball X and Y
+
+    ball_x += 5 * ball_x_orientation
+    ball_y += 5 * ball_y_orientation
+
+    if(ball_x+10 > w) {
+        p1_points++
+        initBall()
+    }
+    else if(ball_x < 0){
+        p2_points ++
+        initBall()
+    }
+
+    if (p1_key == 87 &p1_y > 0) {
+        p1_y -= 10
+    } else if (p1_key == 83 && p1_y + p_h < h) {
+        p1_y += 10
+    }
+
+    if(p2_key == 38 && p2_y > 0){
+        p2_y -= 10
+    }else if(p2_key == 40 && p2_y + p_h < h){
+        p2_y += 10
+    }
+
+
     draw()
 }
 
@@ -48,7 +93,8 @@ function draw() {
    //sidebar
    drawRect(w/2 -5, 0, 5, h)
    //ball
-   drawRect(ball_x, ball_y, 10, 10)
+   drawRect(ball_x, ball_y, 15, 15)
+   writePoints()
 
 }
 
@@ -59,5 +105,26 @@ function drawRect(x, y, w, h, color='#fff') {
     ctx.fillRect (x, y, w, h)
     ctx.fillStyle = '#000'
 }
+
+//Writing points 
+
+function writePonits() {
+    ctx.font = "50px monospace";
+    ctx.fillStyle ="#fff"
+    ctx.fillText(p1_points, w/4, 50);
+    ctx.fillText(p2_points, 3*(w/4), 50);
+}
+
+//Reading move inputs
+
+document.addEventListener ("keydown", function(ev) {
+    if (ev.keyCode == 87 || ev.keyCode == 83) {
+        p1_key = ev.keyCode
+    } else if (ev.keyCode ==38 || ev.keyCode == 40)
+    p2_key = ev.keyCode
+})
+
+
+
 
 setup()
